ddcae1001d1f97fb5faefc46d49ccf63957eaf0d
LeoMangaldeep <105694506+LeoMangaldeep@users.noreply.github.com>
2025-12-06 23:43:59 +1300

    Rename UnitTests

---
+++ PlaywrightTests/UnitTest.cs
@@ -0,0 +1,52 @@
+using Microsoft.Playwright;
+using NUnit.Framework.Internal;
+using PlaywrightTests.Config;
+using PlaywrightTests.Driver;
+
+namespace PlaywrightTests.UnitTest
+{
+
+    public class Tests
+    {
+        private PlaywrightDriver _driver;
+        private PlaywrightDriverInitializer _playwrightDriverInitializer;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            TestSettings testSettings = new TestSettings
+            {
+                Headless = false,
+                DevTools = true,
+                SlowMo = 1500,
+                DriverType = DriverType.Edge
+            };
+            _playwrightDriverInitializer = new PlaywrightDriverInitializer();
+            _driver = new PlaywrightDriver(testSettings, _playwrightDriverInitializer);
+            await _driver.Page.GotoAsync("https://www.qbe.com/");
+        }
+
+        [Test]
+        public async Task TestLinkToAustraliaWebsite()
+        {
+            await _driver.Page.GetByRole(AriaRole.Link, new() { Name = "Visit QBE Australia" }).ClickAsync();
+        }
+
+        [Test]
+        public async Task TestLinkToNewZealandWebsite()
+        {
+            await _driver.Page.GetByRole(AriaRole.Button, new() { Name = "Close" }).ClickAsync();
+            await _driver.Page.GetByText("About").Nth(1).ClickAsync();
+        }
+        
+        [TearDown] 
+        public async Task TearDown()
+        {
+            // Cleanup code needed for playwright browser, browserContext
+            // and dispose the resources, 
+
+            await _driver.Browser.CloseAsync();
+            await _driver.Browser.DisposeAsync();
+        }
+    }
+}
